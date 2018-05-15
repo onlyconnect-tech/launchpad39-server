7fc78d20c3132014d739bab8a7edeab9723e3792
Massimo Cappellano <Massimo.Cappe@gmail.com>
2018-05-15 12:06:10 +0200

    semplification

--- test/redis_notifier.test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const RedisNotifierService = require('../service/redis_notifier');
-
-function onExpires(clientID) {
-    console.log('EXPIRES clientID:', clientID);
-}
-
-const redisNotifierService = new RedisNotifierService(onExpires);
